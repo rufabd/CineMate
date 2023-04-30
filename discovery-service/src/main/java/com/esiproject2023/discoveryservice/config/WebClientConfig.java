8e617c4e97f6f79cb72cb1cf0ffc93fee3f1d246
Rasul Iskandarov <73297277+rasul-isk@users.noreply.github.com>
2023-04-30 21:24:56 +0300

    adding names of services to eureka

--- discovery-service/src/main/java/com/esiproject2023/discoveryservice/config/WebClientConfig.java
+++ discovery-service/src/main/java/com/esiproject2023/discoveryservice/config/WebClientConfig.java
@@ -1,13 +1,15 @@
 package com.esiproject2023.discoveryservice.config;
 
+import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
+@LoadBalancerClient
 public class WebClientConfig {
     @Bean
-    public WebClient webClient() {
-        return WebClient.builder().build();
+    public WebClient.Builder webClient() {
+        return WebClient.builder();
     }
 }
